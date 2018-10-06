d167f519fddb0741c1f49c7342552ef5629c0f51
Martin Ponce <martin.ponce@outlook.com>
2018-10-06 19:39:18 +0800

    Immutable point properties

--- src/actors/grid/point.ts
+++ src/actors/grid/point.ts
@@ -1,3 +1,11 @@
 export class Point {
-  constructor(public x: number, public y: number) {}
+  constructor(private readonly _x: number, private readonly _y: number) {}
+
+  public get x(): number {
+    return this._x;
+  }
+
+  public get y(): number {
+    return this._y;
+  }
 }
